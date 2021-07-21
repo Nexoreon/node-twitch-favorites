334db0852fa24275870f6ee4fb1628ccb637878b
Nexoreon <tiger000226@gmail.com>
2021-07-21 12:25:56 +0300

    improve TWStreamers application

--- apps/TwitchStreamersApp.js
+++ apps/TwitchStreamersApp.js
@@ -5,24 +5,37 @@ const pushNotification = require('../utils/pushNotification')
 const TwitchStreamer = require('../models/twitchStreamerModel')
 const TwitchGame = require('../models/twitchGameModel')
 const TwitchStats = require('../models/twitchStatsModel')
+const { createStats, updateGameHistory, sendNotification } = require('./TwitchCommon')
 
-const year = new Date().getFullYear().toString()
-const month = (new Date().getMonth() + 1).toString()
-const day = new Date().getDate().toString()
+const banStreamer = async user_id => {
+    await TwitchStreamer.findOneAndUpdate({id: user_id}, { // set a cooldown for 6 hours
+        cooldown: Date.now() + 21600000
+    })
+}
+
+const checkBannedStreamers = async () => {
+    const bannedStreamers = await TwitchStreamer.updateMany({cooldown: {$exists: true, $lte: Date.now()}}, {$unset: {cooldown: ''}})
+    if (bannedStreamers.nModified) {
+        console.log(chalk.hex('#a970ff')(`[Twitch Streamers]: ${bannedStreamers.nModified} favorite streamers have been unbanned since ban timer expired`))
+    }
+}
+
+// MAIN PART
 
 const TwitchStreamersApp = async () => {
-    console.log(chalk.hex('#a970ff')(`[Twitch Streamers]: ${new Date(Date.now()).toLocaleString()}`))
-    console.log(chalk.hex('#a970ff')('[Twitch Streamers]: Запуск проверки состояния стримов...'))
+    console.log(chalk.hex('#a970ff')('[Twitch Streamers]: Launching favorite streamers check...', new Date(Date.now()).toLocaleString()))
     try {
+        checkBannedStreamers()
         const streamersStats = await TwitchStats.find()
         const streamersStatsIDs = streamersStats.map(streamer => streamer.userId)
         const games = await TwitchGame.find()
         const gamesIDs = games.map(game => game.id)
         const following = await TwitchStreamer.find() // get streamers from db
         const followingIDs = following.map(streamer => `user_id=${streamer.id}`) // extract streamers id
-        let foundStreams = false
-        let twitchResponse
-    
+
+        let foundStreams = false // defines if there are suitable streams found
+        let twitchResponse // data from twitch will be assigned
+
         try {
             const askTwitch = await axios.get(`https://api.twitch.tv/helix/streams?${followingIDs.join('&')}`, { // make a get request with streamers id
                 headers: {
@@ -30,10 +43,10 @@ const TwitchStreamersApp = async () => {
                     'client-id': process.env.TWITCH_CLIENT
                 }
             })
-            console.log(chalk.hex('#a970ff')('[Twitch Streamers]: Данные успешно получены с сервера Twitch. Обработка...'))
+            console.log(chalk.hex('#a970ff')('[Twitch Streamers]: Successefully got data from Twitch. Processing...'))
             twitchResponse = askTwitch.data.data // set fetched data
         } catch (e) {
-            console.log(chalk.red('[Twitch Streamers]: Ошибка получения актуальной информации о стримах!'), e)
+            console.log(chalk.red('[Twitch Streamers]: Error while getting data from Twitch!'), e)
         }
     
         twitchResponse.map(async streamer => { // handle received array of live streams
@@ -41,58 +54,27 @@ const TwitchStreamersApp = async () => {
                 const findIndex = following.map(str => str.id).indexOf(streamer.user_id) // find array index of streamer
                 streamer.avatar = following[findIndex].avatar // set streamer avatar from db
     
-                if (following[findIndex].cooldown <= Date.now()) { // if streamer has expired cooldown...
-                    console.log(chalk.hex('#a970ff')(`[Twitch Streamers]: Таймер для стримера ${streamer.user_login} вышел. Разблокировка...`))
-                    await TwitchStreamer.findOneAndUpdate({id: streamer.user_id}, { // remove cooldown
-                        cooldown: undefined
-                    })
-                }
-    
                 if (!streamersStatsIDs.includes(streamer.user_id)) {
-                    await TwitchStats.create({
-                        userId: streamer.user_id,
-                        userName: streamer.user_name,
-                        gameId: streamer.game_id,
-                        gameName: streamer.game_name,
-                        viewers: streamer.viewer_count,
-                        title: streamer.title
-                    })
+                    createStats(streamer)
                 }
     
                 if (!following[findIndex].cooldown) { // if streamer doesn't have a cooldown...
-                    console.log(chalk.green(`[Twitch Streamers]: Стример ${streamer.user_name} играет в ${streamer.game_name}. Отправка уведомления...`))
+                    console.log(chalk.green(`[Twitch Streamers]: Streamer ${streamer.user_name} plays ${streamer.game_name}. Sending notification...`))
                     foundStreams = true
-                    pushNotification.publishToInterests(['project'], { // push notification to users
-                        web: {
-                            notification: {
-                                title: `${streamer.game_name}`,
-                                body: `${streamer.user_name} играет в ${streamer.game_name}`,
-                                deep_link: `https://twitch.tv/${streamer.user_login}`,
-                                icon: streamer.avatar
-                            }
-                        }
+                    sendNotification({
+                        title: `${streamer.game_name}`,
+                        message: `${streamer.user_name} plays ${streamer.game_name}`,
+                        link: `https://twitch.tv/${streamer.user_login}`,
+                        icon: streamer.avatar
                     })
-                
-                    await TwitchGame.findOneAndUpdate({id: streamer.game_id}, { // add mark about this event to the game doc
-                        $push: { history: {
-                            streamer_id: streamer.user_id,
-                            streamer: streamer.user_login,
-                            month: month,
-                            year: year,
-                            day: day,
-                            timestamp: Date.now() 
-                        }}
-                    })
-        
-                    await TwitchStreamer.findOneAndUpdate({id: streamer.user_id}, { // set a cooldown for 6 hours
-                        cooldown: Date.now() + 21600000
-                    })
+                    updateGameHistory({stream: streamer, isFavorite: true})
+                    banStreamer(streamer.user_id)
                 }
             }
         })
-        if (foundStreams) console.log(chalk.hex('#a970ff')('[Twitch Streamers]: Подходящих по критериям стримов не найдено'))
+        if (!foundStreams) console.log(chalk.hex('#a970ff')("[Twitch Streamers]: Haven't found any suitable streams"))
     } catch (err) {
-        console.log(chalk.red('[Twitich Streamers]: Произошла ошибка во время получения данных! Операция отменена.', err))
+        console.log(chalk.red('[Twitich Streamers]: Error while executing application! Operation has been cancelled.', err))
     }
 }
 
