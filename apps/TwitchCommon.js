37b6e3d30ece73fa2702eab1d744b390737f0d69
Nexoreon <tiger000226@gmail.com>
2022-03-06 22:46:23 +0300

    add new auxiliary functions

--- apps/TwitchCommon.js
+++ apps/TwitchCommon.js
@@ -1,3 +1,6 @@
+const axios = require('axios')
+const TwitchStreamer = require('../models/miniapps/Twitch/twitchStreamerModel')
+const TwitchWatchlist = require('../models/miniapps/Twitch/twitchWatchlistModel')
 const TwitchGame = require('../models/twitchGameModel')
 const TwitchStats = require('../models/twitchStatsModel')
 const pushNotification = require('../utils/pushNotification')
@@ -38,4 +41,75 @@ exports.sendNotification = ({title, message, link, icon}) => {
             }
         }
     }).then(() => console.log('[Pusher]: Notification has been successefully sent!')).catch(e => console.log(chalk.red('[Pusher]: Error while sending notification!'), e))
+}
+
+exports.createVodSuggestion = async ({ user_id, thumbnail, games }) => {
+    const twitchHeaders = {
+        'client-id': process.env.TWITCH_CLIENT,
+        'Authorization': process.env.TWITCH_TOKEN
+    }
+
+    const getVideo = await axios.get(`https://api.twitch.tv/helix/videos?user_id=${user_id}`, {
+        headers: twitchHeaders
+    })
+    const getFollowers = await axios.get(`https://api.twitch.tv/helix/users/follows?to_id=${user_id}&first=1`, {
+        headers: twitchHeaders
+    })
+
+    const data = getVideo.data.data[0]
+    const followers = getFollowers.data.total
+    const { id, title, user_name: author, created_at: streamDate, url } = data
+
+    await TwitchWatchlist.create({
+        id,
+        title,
+        author, 
+        thumbnail, 
+        games,
+        url,
+        meta: {
+            streamDate,
+            followers
+        },
+        flags: {
+            isSuggestion: true
+        }
+    })
+}
+
+exports.checkActiveGame = async (id, removeJob) => {
+    const removeNotifyingData = async () => {
+        await TwitchStreamer.findOneAndUpdate({ id }, {
+            $set: { 'flags.notifyOnNextGame': false },
+            $unset: { gameName: 1 }
+        })
+        removeJob()
+    }
+
+    const streamer = await TwitchStreamer.findOne({ id }).select({ gameName: 1, avatar: 1 })
+    const response = await axios.get(`https://api.twitch.tv/helix/streams?user_id=${id}`, {
+        headers: {
+            'client-id': process.env.TWITCH_CLIENT,
+            'Authorization': process.env.TWITCH_TOKEN
+        }
+    })
+    const streamData = response.data.data[0]
+
+    if (!streamData) { // if streamer ended the stream, end task and remove game information from document
+        console.log('[Twitch Streamers]: Streamer ended the stream. Removing task...')
+        return removeNotifyingData()
+    }
+
+    if (streamData.game_name !== streamer.gameName) { // if streamer changed the game, send notification and remove this job
+        console.log('[Twitch Streamers]: Streamer started playing another game. Sending notification...')
+        this.sendNotification({
+            title: `${streamData.user_name} started playing another game`,
+            message: `Streamer ${streamData.user_name} started playing ${streamData.game_name}`,
+            icon: streamer.avatar,
+            link: `https://twitch.tv/${streamData.user_login}`      
+        })
+        return removeNotifyingData()
+    } else {
+        console.log(`[Twitch Streamers]: Streamer ${streamData.user_name} still playing current game`)
+    }
 }
