ce38006739fc874e9dfc6067d6887bbf4bce9b3a
Nexoreon <tiger000226@gmail.com>
2022-05-04 16:30:20 +0300

    add a day games history that streamers played

--- apps/TwitchStatsApp.js
+++ apps/TwitchStatsApp.js
@@ -1,8 +1,9 @@
 const chalk = require('chalk')
 const Table = require('cli-table')
 
-const TwitchGame = require('../models/twitchGameModel')
 const TwitchStats = require('../models/twitchStatsModel')
+const TwitchStreamer = require('../models/twitchStreamerModel')
+const TwitchGame = require('../models/twitchGameModel')
 const TwitchReport = require('../models/twitchReportModel')
 const { sendNotification } = require('./TwitchCommon')
 
@@ -10,20 +11,30 @@ const { sendNotification } = require('./TwitchCommon')
 const TwitchStatsApp = async () => {
     console.log(chalk.greenBright('[Twitch Stats]: Launching report creation tool...'))
     try {
+        // FETCH DATA FROM DB
         const stats = await TwitchStats.find() // get twitch stats from db
-        if (!stats.length) return console.log(chalk.greenBright("[Twitch Stats]: No records found in database! Table isn't going to be created")) // if stats has no data, return nothing
+        const followsHistory = await TwitchStreamer.find({ streamHistory: { $exists: 1 }}, { streamHistory: 1, name: 1, _id: 0 })
+        if (!stats.length && !followsHistory.length) return console.log(chalk.greenBright('[Twitch Stats]: Nothing to show! The table will not be generated')) // if stats has no data, return nothing
         const games = await TwitchGame.find() // get favorite games from db
         const gamesIDs = games.map(game => game.id) // extract ids from games db
+
+        // CREATE TABLES
         const tableArray = [] // array that going to be filled with data for the table
         const table = new Table({ // generate table columns
             head: ['Min. viewers', 'Viewers', 'Game', 'Streamer', 'Title'],
             colWidths: [15, 10, 35, 25, 25]
         })
+        const followsTableArray = []
+        const followsTable = new Table({
+            head: ['Streamer', 'Game'],
+            colWidths: [25, 90]
+        })
     
+        // FILL TABLES
         const statsArray = [] // this array going to be filled with the data that will be sent to the reports collection as a report
         stats.map(stream => { // map all stats
             const findIndex = gamesIDs.indexOf(stream.gameId) // find index of game that streamer played
-            const minViewers = games[findIndex].search.minViewers || 2000 // defines minViewers with special value if minViewers field exists, otherwise it will use default 2000
+            const minViewers = games[findIndex].search.minViewers // defines minViewers with special value
             tableArray.push([ minViewers, stream.viewers, stream.gameName, stream.userName, stream.title ]) // push current streamer data to the table array
             statsArray.push({ // push current streamer data to the stats array
                 userId: stream.userId, 
@@ -34,23 +45,35 @@ const TwitchStatsApp = async () => {
                 timestamp: stream.date 
             })
         })
-        console.log(chalk.greenBright('[Twitch Stats]: Everyday report is ready! Adding in database...'))
+
+        const followsHistoryArray = []
+        followsHistory.map(({ streamHistory, name }) => {
+            followsTableArray.push([name, streamHistory.join(', ').toString()])
+            followsHistoryArray.push({ userName: name, games: streamHistory })
+        })
+        console.log(chalk.greenBright('[Twitch Stats]: Daily report is ready! Generating table...'))
+        table.push(...tableArray)
+        followsTable.push(...followsTableArray)
+        console.log(chalk.greenBright('[Twitch Stats]: The table has been successfully generated. Adding report into database...'))
     
+        // CREATE REPORT AND DISPLAY TABLES
         await TwitchReport.create({ // creates daily report with handled data
             timestamp: Date.now(),
-            streams: statsArray
-        }).then(() => console.log(chalk.greenBright('[Twitch Stats]: The report has been added in database. Generating table...'))).catch(e => console.log(chalk.red('[Twitch Stats]: Error while adding report in database!'), e))
+            streams: statsArray,
+            follows: followsHistoryArray
+        })
+        .then(() => console.log(chalk.greenBright('[Twitch Stats]: The report has been added into database. Displaying table and sending notification...')))
+        .catch(e => console.log(chalk.red('[Twitch Stats]: Error while adding report in database!'), e))
     
-        table.push(...tableArray) // generates table
-        console.log(chalk.greenBright('[Twitch Stats]: The table has been successefully generated. Showing table and sending notification...'))
         sendNotification({
             title: 'Streams report is ready',
-            message: `Everyday report is ready! Take a look...`,
+            message: `Daily report is ready! Take a look...`,
             icon: 'https://192.168.0.100/site/MiniApps/TwitchStreamers/icon.jpg', // TODO to be included with the project
-            link: 'https://192.168.0.100/database/dist/mini-apps/twitch-hub' // TODO include frontend application and link
+            link: 'https://192.168.0.100/database/mini-apps/twitch-hub' // TODO include frontend application and link
         })
-        
-        console.log(table.toString())
+        if (followsTable.length) console.log(followsTable.toString())
+        if (table.length) console.log(table.toString())
+        await TwitchStreamer.updateMany({ streamHistory: { $exists: 1 }}, {$unset: { streamHistory: 1 }})
         await TwitchStats.deleteMany({}) // deletes all day stats
     } catch (err) {
         console.log(chalk.red('[Twitch Stats]: Error while creating report! Cancelling operation.'), err)
