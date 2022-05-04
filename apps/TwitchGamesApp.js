63252950b4b32c83d32f999d04e3b67a4a4930bc
Nexoreon <tiger000226@gmail.com>
2022-05-04 16:02:24 +0300

    remove minViewers default value

--- apps/TwitchGamesApp.js
+++ apps/TwitchGamesApp.js
@@ -71,7 +71,7 @@ const TwitchGamesApp = async () => {
         
                 if (!bannedStreamersIDs.includes(stream.user_id) && stream.language === 'en') { // make sure that streams language is english
                     const gameIndex = gamesIDs.indexOf(stream.game_id) // get game id that streamer currently playing
-                    const minViewers = dbGames[gameIndex].search.minViewers || 2000 // if game db field with value minViewers exists, use it instead of default 2000
+                    const minViewers = dbGames[gameIndex].search.minViewers // min amount of viewers required to trigger notification
                     const gameCover = dbGames[gameIndex].boxArt.replace('XSIZExYSIZE', '100x140') // get game box art
                     if (stream.viewer_count >= 1000) {
                         tableArray.push([minViewers, stream.viewer_count, stream.game_name, stream.user_name, stream.title])
