2ee6c79f93c2c85ae115e0bd119747eb3986a079
Nexoreon <tiger000226@gmail.com>
2022-12-02 18:16:43 +0300

    improve reports search query, get today's report

--- controllers/TwitchReportsController.js
+++ controllers/TwitchReportsController.js
@@ -1,21 +1,70 @@
 const TwitchReport = require('../models/twitchReportModel')
+const TwitchStreamer = require('../models/twitchStreamerModel')
+const TwitchStats = require('../models/twitchStatsModel')
 const catchAsync = require('../utils/catchAsync')
 
 exports.getReports = catchAsync(async (req, res, next) => {
-    const { offset, max, streamer, game, date } = req.query
-    const searchPrms = {
-        ...(streamer && {'streams.userName': streamer}),
-        ...(game && {'streams.gameName': game}),
-        ...(date && {'streams.timestamp': date})
+    const { limit, streamer, game } = req.query
+    // SEARCH PARAMS
+    let match = {};
+    if (streamer && !game) {
+        match = { $or: [
+            {...(streamer && !game && {'highlights.userName': {$regex: `^${streamer}`}})},
+            {...(streamer && !game && {'follows.userName': {$regex: `^${streamer}`}})},
+        ]}
     }
-    const reports = await TwitchReport.find(searchPrms).sort({timestamp: -1}).skip(offset * 1).limit(max * 1)
-    const total = await TwitchReport.find(searchPrms)
+    if (game && !streamer) {
+        match = { $or: [
+            {...(game && !streamer && {'highlights.gameName': {$regex: `^${game}`}})},
+            {...(game && !streamer && {'follows.games': {$regex: `^${game}`}})}
+        ]}
+    }
+
+    // QUERY
+    const reports = await TwitchReport.aggregate([
+        { 
+            $match: match
+        },
+        {
+            $lookup: { 
+                from: 'ma_twitch-streamers',
+                pipeline: [
+                    { $project: { avatar: 1, name: 1 }}
+                ],
+                as: 'followList'
+            }
+        },
+        {
+            $sort: { timestamp: -1 }
+        },
+        {
+            $limit: limit * 1
+        }
+    ]);
+    
+    reports.map(async report => {
+        await report.follows.map(streamer => {
+            let user = report.followList.filter(user => {
+                if (user.name === streamer.userName) return user;
+            })[0];
+            if (user && user.avatar) streamer.avatar = user.avatar
+        });
+    });
+
+    // QUERY TODAY REPORT
+    const todayFollows = await TwitchStreamer.find({ 'streamHistory': {$exists: true }}, { userName: '$name', avatar: 1, games: '$streamHistory' });
+    const todayHighlights = await TwitchStats.find();
+    const today = { highlights: todayHighlights, follows: todayFollows };
+    let items = [...reports];
+    if (!game && !streamer) items = [today, ...reports];
+    
+    const total = await TwitchReport.count(match);
 
     res.status(200).json({
         status: 'success',
         data: {
-            items: reports,
-            total: total.length
+            items,
+            total
         }
-    })
-})
+    });
+});
