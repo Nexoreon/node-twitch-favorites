8ec5e5f4761a645ffc470863573e5675b6ec1c50
Nexoreon <tiger000226@gmail.com>
2022-12-02 18:19:06 +0300

    remove score system, get streamer live status

--- controllers/TwitchStreamersController.js
+++ controllers/TwitchStreamersController.js
@@ -1,14 +1,14 @@
 const TwitchStreamer = require('../models/twitchStreamerModel')
 const catchAsync = require('../utils/catchAsync')
 const AppError = require('../utils/appError')
-const axios = require('axios')
+const axios = require('axios');
 
 const { checkActiveGame } = require('../apps/TwitchCommon')
 const { ToadScheduler, SimpleIntervalJob, Task } = require('toad-scheduler')
 const scheduler = new ToadScheduler()
 
 exports.getStreamers = catchAsync(async (req, res, next) => {
-    const streamers = await TwitchStreamer.find().sort({ 'score.current': -1, name: -1 });
+    const streamers = await TwitchStreamer.find().sort({ name: -1 });
 
     const ids = streamers.map(streamer => `user_id=${streamer.id}`);
     const response = await axios.get(`https://api.twitch.tv/helix/streams?${ids.join('&')}`, {
@@ -19,17 +19,18 @@ exports.getStreamers = catchAsync(async (req, res, next) => {
     });
 
     const streamersData = [...streamers]
-    await response.data.data.map(async streamer => {
-        streamersData.map(async oldStreamer => {
-            if (streamer.user_id === oldStreamer.id) {
-                oldStreamer._doc = {
-                    ...oldStreamer._doc,
-                    live: true,
-                    game: streamer.game_name
-                }
+    const liveIDs = await response.data.data.map(streamer => ({ id: streamer.user_id, game: streamer.game_name }));
+    streamersData.map(oldStreamer => {
+            let liveContent
+            liveIDs.find((v,index) => {
+                if (v.id === oldStreamer.id) liveContent = liveIDs[index];
+            });
+            oldStreamer._doc = {
+                ...oldStreamer._doc,
+                live: false,
+                ...(liveContent && { live: true, game: liveContent.game })
             }
-        });
-    });
+    })
 
     res.status(200).json({
         status: 'ok',
@@ -39,10 +40,10 @@ exports.getStreamers = catchAsync(async (req, res, next) => {
 
 exports.getStreamer = catchAsync(async (req, res, next) => {
     const streamer = await TwitchStreamer.findById(req.params.id)
-    if (!streamer) return next(new AppError("This streamer isn't in the favorites list", 404))
+    if (!streamer) return next(new AppError('Такого стримера не найдено в датабазе', 404))
 
     res.status(200).json({
-        status: 'success',
+        status: 'ok',
         data: streamer
     })
 })
@@ -51,7 +52,7 @@ exports.createStreamer = catchAsync(async (req, res, next) => {
     const newStreamer = await TwitchStreamer.create(req.body)
 
     res.status(201).json({
-        status: 'success',
+        status: 'ok',
         data: newStreamer
     })
 })
@@ -81,49 +82,49 @@ exports.updateStreamer = catchAsync(async (req, res, next) => {
     const streamer = await TwitchStreamer.findByIdAndUpdate(id, req.body, {
         new: true,
         multi: true
-    })
+    });
     
-    if (!streamer) return next(new AppError("This streamer isn't in the follow list", 404))
+    if (!streamer) return next(new AppError('Такого стримера не найдено в датабазе', 404));
 
     res.status(200).json({
         status: 'ok',
         data: streamer
-    })
-})
+    });
+});
 
 exports.deleteStreamer = catchAsync(async (req, res, next) => {
     const streamer = await TwitchStreamer.findByIdAndDelete(req.params.id)
-    if (!streamer) return next(new AppError("This streamer isn't in the favorites list", 404))
+    if (!streamer) return next(new AppError('Такого стримера не найдено в датабазе', 404))
 
     res.status(204).json({
-        status: 'success',
-        message: 'Streamer successefully removed'
+        status: 'ok',
+        message: 'Стример успешно удалён'
     })
 })
 
 // Other
 
 exports.notifyOnNextGame = catchAsync(async (req, res, next) => {
-    const { id, duration } = req.query
-    const { game } = req.body
+    const { id, duration } = req.query;
+    const { game } = req.body;
 
     await TwitchStreamer.findOneAndUpdate({ id }, {$set: { // allows to re-add task if server been restarted
         gameName: game,
         'flags.notifyOnNextGame': true
-    }})
+    }});
 
     scheduler.removeById('checkActiveGame') // remove job with this id if exists to avoid conflict
     const task = new Task('checkActiveGame', () => {
-        checkActiveGame(id, () => scheduler.removeById('checkActiveGame'), duration === 'everyGame')
-    })
+        checkActiveGame(id, () => scheduler.removeById('checkActiveGame'), duration === 'everyGame');
+    });
 
     const scheduledTask = new SimpleIntervalJob({ // execute task every 5 minutes
         minutes: 5
-    }, task, 'checkActiveGame')
-    scheduler.addSimpleIntervalJob(scheduledTask)
+    }, task, 'checkActiveGame');
+    scheduler.addSimpleIntervalJob(scheduledTask);
 
     res.status(200).json({
         status: 'ok',
-        message: 'You will be notified when streamer starts to play next game'
-    })
-})
+        message: 'Вы будете оповещены когда стример начнёт играть в следующую игру'
+    });
+});
