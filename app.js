6d12ea635ffb6e53197eb35194f3b8cbf2e5158e
Nexoreon <tiger000226@gmail.com>
2022-12-03 19:20:07 +0300

    remove appRoutes

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ require('./utils/scheduler')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
-const appRoutes = require('./routes/appRoutes')
 const twitchRoutes = require('./routes/twitchRoutes')
 
 const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(cors())
-app.use('/api/v1/app', appRoutes)
 app.use('/api/v1/twitch', twitchRoutes)
 app.all('*', (req, res, next) => {
     next(new AppError(`Unable to find ${req.originalUrl} on the server`, 404))
