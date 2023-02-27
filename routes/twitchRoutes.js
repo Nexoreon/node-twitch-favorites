8526cda3e5f9cc61e20c9a83bf9ae68c1fd25235
Nexoreon <tiger000226@gmail.com>
2023-02-27 13:12:37 +0300

    refactor code

--- routes/twitchRoutes.js
+++ routes/twitchRoutes.js
@@ -1,65 +1,103 @@
-const express = require('express')
-const TwitchStreamersController = require('../controllers/TwitchStreamersController')
-const TwitchGamesController = require('../controllers/TwitchGamesController')
-const TwitchBansController = require('../controllers/TwitchBansController')
-const TwitchReportsController = require('../controllers/TwitchReportsController')
-const TwitchWatchlistController = require('../controllers/TwitchWatchlistController')
+const express = require('express');
+const TwitchStreamersController = require('../controllers/TwitchStreamersController');
+const TwitchGamesController = require('../controllers/TwitchGamesController');
+const TwitchBansController = require('../controllers/TwitchBansController');
+const TwitchReportsController = require('../controllers/TwitchReportsController');
+const TwitchWatchlistController = require('../controllers/TwitchWatchlistController');
 
-const router = express.Router()
-
-// TWITCH STREAMERS
-router.route('/')
-.get(TwitchStreamersController.getStreamers)
-.post(TwitchStreamersController.createStreamer)
-
-router.route('/streamers/notifyOnNextGame')
-.patch(TwitchStreamersController.notifyOnNextGame)
-
-router.route('/streamers/:id')
-.get(TwitchStreamersController.getStreamer)
-.patch(TwitchStreamersController.updateStreamer)
-.delete(TwitchStreamersController.deleteStreamer)
-
-// TWITCH GAMES
-router.route('/games')
-.get(TwitchGamesController.getAllGames)
-.post(TwitchGamesController.createGame)
-
-router.route('/games/:id')
-.get(TwitchGamesController.getGame)
-.patch(TwitchGamesController.updateGame)
-.delete(TwitchGamesController.deleteGame)
-
-router.route('/game/addHistory')
-.patch(TwitchGamesController.addGameHistory)
-
-// TWITCH REPORTS
-router.route('/reports')
-.get(TwitchReportsController.getReports)
+const router = express.Router();
 
 // WATCHLIST
 router.route('/watchlist')
 .get(TwitchWatchlistController.getVideos)
-.post(TwitchWatchlistController.addVideo)
+.post(TwitchWatchlistController.addVideo);
+
+router.route('/getSuggestions')
+.get(TwitchWatchlistController.getSuggestions);
+
+router.route('/watchlist/getParts')
+.get(TwitchWatchlistController.getParts);
 
 router.route('/watchlist/checkVideosAvailability')
-.get(TwitchWatchlistController.checkVideosAvailability)
+.get(TwitchWatchlistController.checkVideosAvailability);
 
 router.route('/watchlist/moveSuggestion')
-.patch(TwitchWatchlistController.moveSuggestion)
+.patch(TwitchWatchlistController.moveSuggestion);
 
 router.route('/watchlist/:id')
+.get(TwitchWatchlistController.getVideo)
 .patch(TwitchWatchlistController.updateVideo)
-.delete(TwitchWatchlistController.deleteVideo)
+.delete(TwitchWatchlistController.deleteVideo);
+
+// TWITCH STREAMERS
+router.route('/')
+.get(TwitchStreamersController.getStreamers)
+.post(TwitchStreamersController.createStreamer);
+
+router.route('/streamers/notifyOnNextGame')
+.patch(TwitchStreamersController.notifyOnNextGame);
+
+router.route('/streamers/:id')
+.get(TwitchStreamersController.getStreamer)
+.patch(TwitchStreamersController.updateStreamer)
+.delete(TwitchStreamersController.deleteStreamer);
+
+// TWITCH GAMES
+router.route('/games')
+.get(TwitchGamesController.getAllGames)
+.post(TwitchGamesController.createGame);
+
+router.route('/games/:id')
+.get(TwitchGamesController.getGame)
+.patch(TwitchGamesController.updateGame)
+.delete(TwitchGamesController.deleteGame);
+
+router.route('/game/addHistory')
+.patch(TwitchGamesController.addGameHistory);
+
+// REPORTS
+router.route('/reports')
+.get(TwitchReportsController.getReports);
 
 // BANNED STREAMERS
 router.route('/banned-streamers/')
 .get(TwitchBansController.getBannedStreamers)
-.post(TwitchBansController.banStreamer)
+.post(TwitchBansController.banStreamer);
 
 router.route('/banned-streamers/:id')
 .get(TwitchBansController.getBannedStreamer)
 .delete(TwitchBansController.unbanStreamer)
-.patch(TwitchBansController.editBan)
+.patch(TwitchBansController.editBan);
 
-module.exports = router
+// UTILS
+router.route('/utils/getVodsData')
+.patch(TwitchUtilsController.getVodsData);
+
+router.route('/utils/checkReports')
+.get(TwitchUtilsController.checkReports);
+
+router.route('/utils/createReportsBackup')
+.get(TwitchUtilsController.createReportsBackup);
+
+router.route('/utils/importFollowList')
+.get(TwitchUtilsController.importFollowList);
+
+router.route('/utils/searchGame')
+.get(TwitchUtilsController.searchGame);
+
+router.route('/utils/searchStreamer')
+.get(TwitchUtilsController.searchStreamer);
+
+router.route('/utils/resetNotificationStatus')
+.get(TwitchUtilsController.resetNotificationStatus);
+
+router.route('/utils/checkStreamersActivity')
+.get(TwitchStreamersController.checkStreamersActivity);
+
+router.route('/utils/checkGamesActivity')
+.get(TwitchGamesController.checkGamesActivity);
+
+router.route('/utils/createDailyReport')
+.post(TwitchReportsController.createDailyReport);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-const mongoose = require('mongoose')
-const app = require('./app')
+const mongoose = require('mongoose');
+const app = require('./app');
 
 process.on('uncaughtException', err => {
-    console.log('UNCAUGHT EXCEPTION! Shutting Down...', err)
-    process.exit(1)
-})
+    console.log('UNCAUGHT EXCEPTION! Shutting Down...', err);
+    process.exit(1);
+});
 
 mongoose.connect(process.env.DB_URL)
 .then(() => console.log('Successful connection to database!'))
-.catch(err => console.log('Error while connecting to dabase!', err))
+.catch(err => console.log('Error while connecting to dabase!', err));
 
 const server = app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server has been launched and listening on port: ${process.env.PORT}`)
-})
+    console.log(`Server has been launched and listening on port: ${process.env.PORT}`);
+});
 
 process.on('unhandledRejection', err => {
-    console.log('UNHANDLED REJECTION! Shutting Down...', err)
+    console.log('UNHANDLED REJECTION! Shutting Down...', err);
     server.close(() => {
-        process.exit(1)
-    })
-})
+        process.exit(1);
+    });
+});
 
 process.on('SIGTERM', () => { // required in Heroku, since servers restarts every 24 hours
-    console.log('SIGTERM RECEIVED! Shutting down...')
+    console.log('SIGTERM RECEIVED! Shutting down...');
     server.close(() => {
-        process.exit(1)
-    })
-})
+        process.exit(1);
+    });
+});
