42c5602d9d782e27ad23336459b8bf7d4abec0d3
Nexoreon <tiger000226@gmail.com>
2023-02-28 14:13:27 +0300

    improve applications

--- apps/TwitchGamesApp.js
+++ apps/TwitchGamesApp.js
@@ -1,53 +1,36 @@
-const axios = require('axios')
-const chalk = require('chalk')
-const Table = require('cli-table')
+const axios = require('axios');
+const chalk = require('chalk');
+const Table = require('cli-table');
 
-const TwitchGame = require('../models/twitchGameModel')
-const TwitchBan = require('../models/twitchBanModel')
-const TwitchStats = require('../models/twitchStatsModel')
-const TwitchStreamer = require('../models/twitchStreamerModel')
-const { updateGameHistory, createStats, sendNotification, createVodSuggestion } = require('./TwitchCommon')
-
-const checkBannedStreamers = async () => { // checks if banned streamer timer expired
-    await TwitchBan.deleteMany({permanent: false, expiresIn: {$lte: Date.now()}})
-    .then(unbanned => unbanned.deletedCount ? console.log(chalk.yellowBright(`[Twitch Games]: ${unbanned.deletedCount} стримера было разблокировано в связи с истечением срока бана`)) : null)
-    .catch(err => console.log(chalk.red('[Twitch Games]: Произошла ошибка во время выполнения приложения! Операция отменена.'), err))
-}
-
-const banStreamer = async stream => {
-    await TwitchBan.create({ // set a cooldown for this streamer for 6 hours
-        userId: stream.user_id,
-        userName: stream.user_name,
-        game: stream.game_name,
-        viewers: stream.viewer_count,
-        reason: '[Twitch Games]: Automated temporary ban',
-        expiresIn: Date.now() + 21600000
-    })
-}
+const TwitchGame = require('../models/twitchGameModel');
+const TwitchBan = require('../models/twitchBanModel');
+const TwitchStats = require('../models/twitchStatsModel');
+const TwitchStreamer = require('../models/twitchStreamerModel');
+const { banStreamer, checkBannedStreamers, updateGameHistory, createStats, sendNotification, createVodSuggestion } = require('./TwitchCommon');
 
 const TwitchGamesApp = async () => {
-    console.log(chalk.yellowBright('[Twitch Games]: Запуск проверки игр на Twitch...'), new Date(Date.now()).toLocaleString())
+    console.log(chalk.yellowBright('[Twitch Games]: Запуск проверки игр на Twitch...'), new Date(Date.now()).toLocaleString());
     try {
-        checkBannedStreamers()
-        const dbStreamersStats = await TwitchStats.find()
-        const streamersStatsIDs = dbStreamersStats.map(streamer => streamer.userId)
+        checkBannedStreamers();
+        const dbStreamersStats = await TwitchStats.find();
+        const streamersStatsIDs = dbStreamersStats.map(streamer => streamer.userId);
         
-        const dbBannedStreamers = await TwitchBan.find() // get banned streamers from db (streamers getting banned for 6 hours if they already been shown to the user)
-        const bannedStreamersIDs = dbBannedStreamers.map(streamer => streamer.userId) // convert to banned streamers ids
+        const dbBannedStreamers = await TwitchBan.find(); // get banned streamers from db (streamers getting banned for 6 hours if they already been shown to the user)
+        const bannedStreamersIDs = dbBannedStreamers.map(streamer => streamer.userId); // convert to banned streamers ids
         
-        const dbFavoriteStreamers = await TwitchStreamer.find()
-        const favoriteStreamersIDs = dbFavoriteStreamers.map(streamer => streamer.id)
+        const dbFavoriteStreamers = await TwitchStreamer.find();
+        const favoriteStreamersIDs = dbFavoriteStreamers.map(streamer => streamer.id);
 
-        const dbGames = await TwitchGame.find({ 'search.isSearchable': true }) // get favorite games from db
-        const gamesIDs = dbGames.map(game => game.id) // convert to games ids
-        const getGamesIDs = dbGames.map(game => `game_id=${game.id}`) // convert ids for http request 
-        let twitchResponse
+        const dbGames = await TwitchGame.find({ 'search.isSearchable': true }); // get favorite games from db
+        const gamesIDs = dbGames.map(game => game.id); // convert to games ids
+        const getGamesIDs = dbGames.map(game => `game_id=${game.id}`); // convert ids for http request 
+        let twitchResponse;
     
         const table = new Table({
             head: ['Мин. зрителей', 'Всего зрителей', 'Игра', 'Стример', 'Заголовок'],
             colWidths: [15, 15, 25, 25, 27]
-        })
-        const tableArray = []
+        });
+        const tableArray = [];
 
         try {
             const askTwitch = await axios.get(`https://api.twitch.tv/helix/streams?first=60&${getGamesIDs.join('&')}`, { // make a request to twitch api
@@ -55,56 +38,56 @@ const TwitchGamesApp = async () => {
                     'Authorization': process.env.TWITCH_TOKEN,
                     'client-id': process.env.TWITCH_CLIENT
                 }
-            })
-            console.log(chalk.yellowBright('[Twitch Games]: Данные о стримах успешно получены. Обработка...'))
+            });
+            console.log(chalk.yellowBright('[Twitch Games]: Данные о стримах успешно получены. Обработка...'));
     
-            twitchResponse = askTwitch.data.data // set fetched data from twitch api (fetched data contains streamers that are currently playing previously specified games)
+            twitchResponse = askTwitch.data.data; // set fetched data from twitch api (fetched data contains streamers that are currently playing previously specified games)
         } catch (e) {
-            console.log(chalk.red('[Twitch Games]: Ошибка получения данных о стримах!'), e)
+            console.log(chalk.red('[Twitch Games]: Ошибка получения данных о стримах!'), e);
         }
     
         twitchResponse.map(async stream => { // map all streams
             if (!bannedStreamersIDs.includes(stream.user_id) && !favoriteStreamersIDs.includes(stream.user_id) && gamesIDs.includes(stream.game_id)) { // if streamer id is in the banned list or favorite list skip him. TWITCH API BUG FIX APPLIED: Check if game id exists in followed games ids list
                 if (!streamersStatsIDs.includes(stream.user_id) && stream.language === 'en' && stream.viewer_count >= 1000 || !streamersStatsIDs.includes(stream.user_id) && stream.language === 'ru' && stream.viewer_count >= 2000) {
-                    createStats(stream)
+                    createStats(stream);
                 }
         
                 if (!bannedStreamersIDs.includes(stream.user_id) && stream.language === 'en' || !bannedStreamersIDs.includes(stream.user_id) && stream.language === 'ru') { // make sure that streams language is russian or english
-                    const gameIndex = gamesIDs.indexOf(stream.game_id) // get game id that streamer currently playing
-                    const minViewers = dbGames[gameIndex].search.minViewers // min amount of viewers required to trigger notification
-                    const gameCover = dbGames[gameIndex].boxArt.replace('XSIZExYSIZE', '100x140') // get game box art
+                    const gameIndex = gamesIDs.indexOf(stream.game_id); // get game id that streamer currently playing
+                    const minViewers = dbGames[gameIndex].search.minViewers; // min amount of viewers required to trigger notification
+                    const gameCover = dbGames[gameIndex].boxArt.replace('XSIZExYSIZE', '100x140'); // get game box art
                     if (stream.viewer_count >= 1000) {
-                        tableArray.push([minViewers, stream.viewer_count, stream.game_name, stream.user_name, stream.title])
+                        tableArray.push([minViewers, stream.viewer_count, stream.game_name, stream.user_name, stream.title]);
                     }
         
                     if (stream.viewer_count >= minViewers) { // if streamer has more viewers than specified in minViewers variable...
-                        console.log(chalk.yellowBright(`Найден стример ${stream.user_name} который играет в ${stream.game_name} с ${stream.viewer_count} зрителями. Отсылка уведомления...`))
+                        console.log(chalk.yellowBright(`Найден стример ${stream.user_name} который играет в ${stream.game_name} с ${stream.viewer_count} зрителями. Отсылка уведомления...`));
                         sendNotification({
                             title: stream.game_name,
                             message: `${stream.user_name} играет в ${stream.game_name} с ${stream.viewer_count} зрителями`,
                             link: `https://twitch.tv/${stream.user_login}`,
                             icon: gameCover
-                        })
+                        });
                         createVodSuggestion({
                             user_id: stream.user_id,
                             games: [stream.game_name]
-                        })
-                        updateGameHistory({stream, isFavorite: false})
-                        banStreamer(stream)
+                        });
+                        updateGameHistory({stream, isFavorite: false});
+                        banStreamer(stream);
                     }
                 }
             }
-        })
+        });
     
-        table.push(...tableArray)
+        table.push(...tableArray);
         if (table.length) {
-            console.log(table.toString())
+            console.log(table.toString());
         } else {
-            console.log(chalk.yellowBright('[Twitch Games]: Подходящих по критериям стримов не найдено! Таблица составлена не будет'))
+            console.log(chalk.yellowBright('[Twitch Games]: Подходящих по критериям стримов не найдено! Таблица составлена не будет'));
         }
     } catch (e) {
-        console.log(chalk.red('[Twitch Games]: Произошла ошибка во время выполнения приложения! Операция отменена.'), e)
+        console.log(chalk.red('[Twitch Games]: Произошла ошибка во время выполнения приложения! Операция отменена.'), e);
     }
-}
+};
 
-module.exports = TwitchGamesApp
+module.exports = TwitchGamesApp;
--- apps/TwitchStreamersApp.js
+++ apps/TwitchStreamersApp.js
@@ -1,23 +1,12 @@
-const axios = require('axios')
-const chalk = require('chalk')
-const pushNotification = require('../utils/pushNotification')
+const axios = require('axios');
+const chalk = require('chalk');
+const pushNotification = require('../utils/pushNotification');
 
-const TwitchStreamer = require('../models/twitchStreamerModel')
-const TwitchGame = require('../models/twitchGameModel')
-const TwitchStats = require('../models/twitchStatsModel')
-const { createStats, updateGameHistory, sendNotification, createVodSuggestion } = require('./TwitchCommon')
-
-const banStreamer = async user_id => {
-    await TwitchStreamer.findOneAndUpdate({id: user_id}, { // set a cooldown for 6 hours
-        cooldown: Date.now() + 21600000
-    })
-}
-
-const checkBannedStreamers = async () => {
-    await TwitchStreamer.updateMany({cooldown: {$exists: true, $lte: Date.now()}}, {$unset: {cooldown: ''}})
-    .then(banned => banned.modifiedCount ? console.log(chalk.hex('#a970ff')(`[Twitch Streamers]: ${banned.modifiedCount} избранных стримеров были разблокированы в связи с истечением срока бана`)) : null)
-    .catch(err => console.log(chalk.red('[Twitich Streamers]: Произошла ошибка во время получения данных! Операция отменена.'), err))
-}
+const TwitchStreamer = require('../models/twitchStreamerModel');
+const TwitchGame = require('../models/twitchGameModel');
+const TwitchStats = require('../models/twitchStatsModel');
+const TwitchBan = require('../models/twitchBanModel');
+const { banStreamer, checkBannedStreamers, createStats, updateGameHistory, sendNotification, createVodSuggestion } = require('./TwitchCommon');
 
 const addToStreamHistory = async (userId, gameName) => {
     await TwitchStreamer.updateOne({ id: userId }, {
@@ -27,18 +16,20 @@ const addToStreamHistory = async (userId, gameName) => {
 
 // MAIN PART
 const TwitchStreamersApp = async () => {
-    console.log(chalk.hex('#a970ff')('[Twitch Streamers]: Запуск проверки избранных стримеров...', new Date(Date.now()).toLocaleString()))
+    console.log(chalk.hex('#a970ff')('[Twitch Streamers]: Запуск проверки избранных стримеров...', new Date(Date.now()).toLocaleString()));
     try {
-        checkBannedStreamers()
-        const streamersStats = await TwitchStats.find()
-        const streamersStatsIDs = streamersStats.map(streamer => streamer.userId)
-        const games = await TwitchGame.find()
-        const gamesIDs = games.map(game => game.id)
-        const following = await TwitchStreamer.find() // get streamers from db
-        const followingIDs = following.map(streamer => `user_id=${streamer.id}`) // extract streamers id
+        checkBannedStreamers();
+        const streamersStats = await TwitchStats.find();
+        const streamersStatsIDs = streamersStats.map(streamer => streamer.userId);
+        const bannedStreamers = await TwitchBan.find();
+        const bannedStreamersIDs = bannedStreamers.map(streamer => streamer.userId);
+        const games = await TwitchGame.find();
+        const gamesIDs = games.map(game => game.id);
+        const following = await TwitchStreamer.find(); // get streamers from db
+        const followingIDs = following.map(streamer => `user_id=${streamer.id}`); // extract streamers id
 
-        let foundStreams = false // defines if there are suitable streams found
-        let twitchResponse // data from twitch will be assigned
+        let foundStreams = false; // defines if there are suitable streams found
+        let twitchResponse; // data from twitch will be assigned
 
         try {
             const askTwitch = await axios.get(`https://api.twitch.tv/helix/streams?${followingIDs.join('&')}`, { // make a get request with streamers id
@@ -46,47 +37,44 @@ const TwitchStreamersApp = async () => {
                     'Authorization': process.env.TWITCH_TOKEN,
                     'client-id': process.env.TWITCH_CLIENT
                 }
-            })
-            console.log(chalk.hex('#a970ff')('[Twitch Streamers]: Данные успешно получены с сервера Twitch. Обработка...'))
-            twitchResponse = askTwitch.data.data // set fetched data
+            });
+            console.log(chalk.hex('#a970ff')('[Twitch Streamers]: Данные успешно получены с сервера Twitch. Обработка...'));
+            twitchResponse = askTwitch.data.data; // set fetched data
         } catch (e) {
-            console.log(chalk.red('[Twitch Streamers]: Ошибка получения актуальной информации о стримах!'), e)
+            console.log(chalk.red('[Twitch Streamers]: Ошибка получения актуальной информации о стримах!'), e);
         }
     
         twitchResponse.map(async streamer => { // handle received array of live streams
-            const index = following.map(str => str.id).indexOf(streamer.user_id) // find array index of streamer
+            const index = following.map(str => str.id).indexOf(streamer.user_id); // find array index of streamer
             const streamerData = following[index];
 
-            if (!streamerData.streamHistory.includes(streamer.game_name) && streamer.game_name !== 'Just Chatting') addToStreamHistory(streamer.user_id, streamer.game_name)
+            if (!streamerData.streamHistory.includes(streamer.game_name) && streamer.game_name !== 'Just Chatting') addToStreamHistory(streamer.user_id, streamer.game_name);
             if (gamesIDs.includes(streamer.game_id)) { // if streamer plays one of the favorite games...
-                streamer.avatar = following[index].avatar // set streamer avatar from db
+                streamer.avatar = following[index].avatar; // set streamer avatar from db
     
-                if (!streamersStatsIDs.includes(streamer.user_id)) {
-                    createStats(streamer)
-                }
-    
-                if (!following[index].cooldown) { // if streamer doesn't have a cooldown...
-                    console.log(chalk.green(`[Twitch Streamers]: Стример ${streamer.user_name} играет в ${streamer.game_name}. Отправка уведомления...`))
-                    foundStreams = true
+                if (!streamersStatsIDs.includes(streamer.user_id)) createStats(streamer);
+                if (!bannedStreamersIDs.includes(streamer.user_id)) { // if streamer doesn't have a cooldown...
+                    console.log(chalk.green(`[Twitch Streamers]: Стример ${streamer.user_name} играет в ${streamer.game_name}. Отправка уведомления...`));
+                    foundStreams = true;
                     sendNotification({
                         title: `${streamer.game_name}`,
                         message: `${streamer.user_name} играет в ${streamer.game_name}`,
                         link: `https://twitch.tv/${streamer.user_login}`,
                         icon: streamer.avatar
-                    })
+                    });
                     createVodSuggestion({
                         user_id: streamer.user_id,
                         games: [streamer.game_name]
-                    })
-                    updateGameHistory({stream: streamer, isFavorite: true})
-                    banStreamer(streamer.user_id)
+                    });
+                    updateGameHistory({stream: streamer, isFavorite: true});
+                    banStreamer(streamer);
                 }
             }
-        })
-        if (!foundStreams) console.log(chalk.hex('#a970ff')('[Twitch Streamers]: Подходящих по критериям стримов не найдено'))
+        });
+        if (!foundStreams) console.log(chalk.hex('#a970ff')('[Twitch Streamers]: Подходящих по критериям стримов не найдено'));
     } catch (err) {
-        console.log(chalk.red('[Twitich Streamers]: Произошла ошибка во время получения данных! Операция отменена.', err))
+        console.log(chalk.red('[Twitich Streamers]: Произошла ошибка во время получения данных! Операция отменена.', err));
     }
-}
+};
 
-module.exports = TwitchStreamersApp
+module.exports = TwitchStreamersApp;
