2bd8f7b2120245d50c52549bcce4ccc9d23aad73
Nexoreon <tiger000226@gmail.com>
2023-08-24 16:18:06 +0300

    improve error handling

--- server.js
+++ server.js
@@ -3,16 +3,15 @@ const https = require('https');
 const chalk = require('chalk');
 const mongoose = require('mongoose');
 const app = require('./app');
+const Settings = require('./models/settingsModel');
+const telegramBot = require('./utils/TelegramBot');
 
-process.on('uncaughtException', err => {
-    console.log('UNCAUGHT EXCEPTION! Shutting Down...', err);
-    process.exit(1);
-});
-
+// CONNECT TO DATABASE
 mongoose.connect(process.env.DB_URL)
 .then(() => console.log(chalk.green('[Датабаза]: Успешное соединение с датабазой!')))
 .catch(err => console.log('[Датабаза]: Ошибка соединения с датабазой!', err));
 
+// START SERVER
 let server;
 const successMsg = chalk.green(`[Сервер]: Успешный запуск. Сервер прослушивается на порту: ${process.env.PORT}`)
 if (process.env.PORT * 1 === 9500) {
@@ -31,14 +30,43 @@ if (process.env.PORT * 1 === 9500) {
     });
 }
 
-process.on('unhandledRejection', err => {
-    console.log('UNHANDLED REJECTION! Shutting Down...', err);
-    server.close(() => {
-        process.exit(1);
-    });
+// SHUTDOWN SERVER ON ERROR
+const shutdownServer = async err => {
+    const settings = await Settings.find();
+    const { notifications } = settings[0];
+
+    if (notifications.error.telegram) {
+        const text = `Критическая ошибка! Приложение было остановлено из за произошедшей ошибки
+<code>${err}</code>`
+        new Promise(async (resolve, reject) => {
+            await telegramBot.sendMessage(process.env.TELEGRAM_MY_ID * 1, text, {
+            parse_mode: 'HTML'
+            });
+            setTimeout(resolve, 1000);
+        })
+        .finally(() => {
+            server.close(() => {
+                process.exit(1);
+            });
+        })
+    } else {
+        server.close(() => {
+            process.exit(1);
+        });
+    }
+};
+
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION! Shutting Down...', err);
+    shutdownServer(err)
 });
 
-process.on('SIGTERM', () => { // required in Heroku, since servers restarts every 24 hours
+process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION! Shutting Down...', err);
+    shutdownServer(err);
+});
+
+process.on('SIGTERM', () => {
     console.log('SIGTERM RECEIVED! Shutting down...');
     server.close(() => {
         process.exit(1);
